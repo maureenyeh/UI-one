86c698a9d0da16bb6720349c300434510a01497f
meiying <rita00557@hotmail.com>
2023-08-29 10:30:46 +0800

    add avatar bar

--- ui_one/lib/main.dart
+++ ui_one/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart' as badges;
 
 void main() {
   runApp(const MainApp());
@@ -11,12 +12,82 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Row(
-          children: [
-            CircleAvatar(
-              backgroundImage: AssetImage('assets/profile.jpg'),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
+          child: SafeArea(
+            child: Container(
+              height: 50,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage('assets/profile.jpg'),
+                      radius: 25,
+                    ),
+                  ),
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text(
+                            'Rafel Williams',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Text(
+                            'Good morning',
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      // decoration: BoxDecoration(border: Border.all()),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Center(
+                            child: Container(
+                              // decoration: BoxDecoration(border: Border.all()),
+                              child: badges.Badge(
+                                position: badges.BadgePosition.topEnd(
+                                    top: -20, end: -20),
+                                badgeContent: Text(
+                                  '4',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                badgeStyle: badges.BadgeStyle(
+                                  badgeColor: Color.fromARGB(255, 62, 232, 223),
+                                  padding: EdgeInsets.all(8),
+                                  // elevation: 20,
+                                ),
+                                child: Icon(
+                                  Icons.notifications,
+                                  size: 30,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ],
+          ),
         ),
         bottomNavigationBar: BottomAppBar(
           shape: const CircularNotchedRectangle(),
@@ -26,7 +97,7 @@ class MainApp extends StatelessWidget {
           onPressed: () {},
           child: const Icon(Icons.add),
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       ),
     );
   }
